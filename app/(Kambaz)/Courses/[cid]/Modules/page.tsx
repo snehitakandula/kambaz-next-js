ce3e7f1cd616b1ac5eec52d506c6998166eadfb8
DESKTOP-4KJGEIU\snehi <kandula.sne@northeastern.edu>
2025-09-22 22:04:42 -0400

    hwk2-2.2.2

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -161,29 +161,8 @@ export default function CourseHome() {
         </ul>
       </div>
 
-      {/* Right Sidebar: Course Status */}
-      <div
-        style={{
-          flex: 1,
-          borderLeft: "1px solid #ccc",
-          paddingLeft: "15px",
-        }}
-      >
-        <h3>Course Status</h3>
-        <div style={{ marginBottom: "10px" }}>
-          <button>Unpublish</button>
-          <button>Publish</button>
-        </div>
-        <div style={{ display: "flex", flexDirection: "column", gap: "6px" }}>
-          <button>Import Existing Content</button>
-          <button>Import from Commons</button>
-          <button>Choose Home Page</button>
-          <button>View Course Stream</button>
-          <button>New Announcement</button>
-          <button>New Analytics</button>
-          <button>View Course Notifications</button>
-        </div>
-      </div>
+     
+      
     </div>
   );
 }
