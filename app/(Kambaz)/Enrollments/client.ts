5360146c15c5842c719fc26caf3c3ae78741c563
DESKTOP-4KJGEIU\snehi <kandula.sne@northeastern.edu>
2025-11-22 02:13:06 -0500

    enroll/unenroll error

--- app/(Kambaz)/Enrollments/client.ts
+++ app/(Kambaz)/Enrollments/client.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const REMOTE_SERVER = process.env.NEXT_PUBLIC_REMOTE_SERVER || "http://localhost:4000";
-const ENROLLMENTS_API = `${REMOTE_SERVER}/api/enrollments`;
+export const HTTP_SERVER =
+  process.env.NEXT_PUBLIC_HTTP_SERVER ||
+  (typeof window !== "undefined"
+    ? `${window.location.protocol}//${window.location.hostname}:4000`
+    : "http://localhost:4000");
+const ENROLLMENTS_API = `${HTTP_SERVER}/api/enrollments`;
 
 export const enrollCourse = async (userId: string, courseId: string) => {
   const response = await axios.post(`${ENROLLMENTS_API}/${userId}/${courseId}`);
